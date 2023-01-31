0ba07c10f0e765844f8a7842a51c7d46062165ef
Abdelrahman Youssef <Youssef@Theas-MacBook-Air.local>
2023-01-31 13:33:19 -0500

    Work on Button component and controls

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Button.css';
+const Button=(
+	className,
+	raised,
+	unelevated,
+	outlined,
+	dense,
+	notCased,
+	disabled,
+	icon,
+	iconClass,
+	href,
+	onClick,
+	children
+) =>{
+	const CSS_classes = {
+		Root: 'Button',
+		ICON: 'Button__Icon',
+		LABEL: 'Button__Label',
+		DENSE: 'Button_dense',
+		RAISED: 'Button_raised',
+		OUTLINED: 'Button_outlined',
+		UNELEVATED: 'Button_unelevated',
+		UPPERCASE: 'Button_uppercase',
+    };
+    const Class_builder=(rootClass, classMapping,)
+	return <div>Button</div>;
+}
+
+export default Button;
---
+++ src/Components/Visualizer/Visualizer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './visualizer.css';
+function Visualizer() {
+	return <div>Visualizer</div>;
+}
+
+export default Visualizer;
